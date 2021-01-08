bd78e27e97a323b725bcea2898243ca70c79267f
MarcosVinicius2003 <marvinicius78910@gmail.com>
2021-01-07 21:00:28 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Classes__métodos_e_atributos_estáticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
